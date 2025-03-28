dff3d07bea437527f73d101475e999de7cdd005e
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-03-28 10:28:00 -0500

    Camel Case class names

--- src/main/java/sh/jfm/fasterjavademo/FasterJavaDemoApplication.java
+++ src/main/java/sh/jfm/fasterjavademo/FasterJavaDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FasterjavademoApplication {
+public class FasterJavaDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FasterjavademoApplication.class, args);
+		SpringApplication.run(FasterJavaDemoApplication.class, args);
 	}
 
 }
