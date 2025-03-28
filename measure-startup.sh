e9fb0403dcd6a6b2c90ea6a9197c91ee29708c7d
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-03-28 16:20:47 -0500

    WIP Experiment running container health checks

--- measure-startup.sh
+++ measure-startup.sh
@@ -16,12 +16,26 @@ fi
 # Run the container in the background
 docker run --rm -d --name "$CONTAINER_NAME" -p 8080:8080 -e JAVA_OPTS="-Xms512m -Xmx512m" "$IMAGE_NAME"
 
+#docker run --rm -d \
+#  --name "$CONTAINER_NAME" \
+#  --health-cmd="curl --fail --silent localhost:8080/actuator/health | grep UP || exit 1" \
+#  --health-interval=10s \
+#  --health-timeout=5s \
+#  --health-retries=3 \
+#  -p 8080:8080 "$IMAGE_NAME"
+
 # Wait for the app to be healthy
 echo "Waiting for application to become healthy..."
 until curl -fs http://localhost:8080/actuator/health | grep -q '"status":"UP"'; do
     sleep 1
 done
 echo "Application is healthy"
+
+#echo "Waiting for container health..."
+#until [ "$(docker inspect --format='{{.State.Health.Status}}' $CONTAINER_NAME)" == "healthy" ]; do
+#    sleep 1
+#done
+#echo "Application is healthy!"
 
 # Get the startup time
 STARTUP_TIME=$(curl -s http://localhost:8080/actuator/metrics/application.ready.time | jq '.measurements[0].value')
