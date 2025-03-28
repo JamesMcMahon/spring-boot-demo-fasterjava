14f0af986e432c52f92251c82d4006f0726769be
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-03-28 10:15:32 -0500

    Spring Init

---
+++ src/main/java/sh/jfm/fasterjavademo/FasterjavademoApplication.java
@@ -0,0 +1,13 @@
+package sh.jfm.fasterjavademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FasterjavademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FasterjavademoApplication.class, args);
+	}
+
+}
